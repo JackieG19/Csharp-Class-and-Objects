1396a6dfb6f65bc8ce372bd21932971e2df6e0e1
Jackie <JGtheophila19@gmail.com>
2020-01-20 20:26:10 -0500

    cresate a person object from a string

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -5,13 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 /*using statement*/
 
-namespace HelloWorld
+namespace Classes
 {
+    public string Name; // field
+    
+    public void Introduce(tring to) // method
+    {
+        // {this are parameter that will be filled by some value}
+        Console.WriteLine("Hi {0}, I am {1}", to, Name); // format string 
+    }
+    
+    public Person Parse(string str)
+    {
+        var person = new Person();
+        person.Name = str;
+        
+        return person;
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //var person = new Person();
+            //var p = person.Parse("John");
+            
+            var person = Person.Parse("John");
+            person.Introduce("Mosh");
         }
     }
 }
