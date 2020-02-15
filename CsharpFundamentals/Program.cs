278620d534a3d75fed7106ee27d8481591ecd4e7
Jackie <JGtheophila19@gmail.com>
2020-02-15 10:29:05 -0500

    adding a new customer

--- CsharpFundamentals/Program.cs
+++ CsharpFundamentals/Program.cs
@@ -1,37 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*using statement*/
 
-namespace Classes
-{
-    public string Name; // field
-    
-    public void Introduce(tring to) // method
-    {
-        // {this are parameter that will be filled by some value}
-        Console.WriteLine("Hi {0}, I am {1}", to, Name); // format string 
-    }
-    
-    public Person Parse(string str)
-    {
-        var person = new Person();
-        person.Name = str;
-        
-        return person;
-    }
-    
+
+namespace CsharpFundamentals
+{    
     class Program
     {
         static void Main(string[] args)
         {
-            //var person = new Person();
-            //var p = person.Parse("John");
+            //var customer = new Customer(1, "John");
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
             
-            var person = Person.Parse("John");
-            person.Introduce("Mosh");
+            var order = new Order();
+            customer.Orders.Add(order);
+            
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
         }
     }
 }
